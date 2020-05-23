bff95ad02dd7114525f63cca5af7c5cc2e92061d
Tomohiro Komiyama <profcoder810@gmail.com>
2020-05-24 02:53:59 +0800

    reinited: backup - files

---
+++ book-form.php
@@ -0,0 +1,25 @@
+<?php
+
+if(isset($_POST['submit'])){
+    $name = $_POST ['name'];
+    $number = $_POST['number'];
+    $email = $_POST['email'];
+    $tretman = $_POST['tretman'];
+    $dan = $_POST['dan'];
+    $month = $_POST['month'];
+    $godina = $_POST['godina'];
+    $vreme = $_POST['vreme'];
+
+    $to = "info@salonspaj.com";
+    $headers = "From:".$email;
+    $txt = "you have received an email from ".$name.".\n\n".$message
+    mail();
+
+  
+
+mail($to,  $name);
+header("Location:book.html");
+
+}
+
+?>
